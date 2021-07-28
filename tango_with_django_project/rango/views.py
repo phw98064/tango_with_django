4818ae916b14f95a7992755ca86bf44f1918c9e2
Haowei Pei <2359365350@Qq.com>
2021-07-28 23:27:26 +0100

    chapter4 complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,7 +4,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
+    # 构建一个字典，作为上下文传给模板引擎
+    # 注意，boldmessage 键对应于模板中的 {{ boldmessage }}
+    #context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # 返回一个渲染后的响应发给客户端
+    # 为了方便，我们使用的是 render 函数的简短形式
+    # 注意，第二个参数是我们想使用的模板
+    return render(request, 'rango/about.html')
+
 
 def index(request):
     return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+def index(request):
+    # 构建一个字典，作为上下文传给模板引擎
+    # 注意，boldmessage 键对应于模板中的 {{ boldmessage }}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # 返回一个渲染后的响应发给客户端
+    # 为了方便，我们使用的是 render 函数的简短形式
+    # 注意，第二个参数是我们想使用的模板
+    return render(request, 'rango/index.html', context=context_dict)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_ROOT = MEDIA_DIR
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
