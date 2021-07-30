1c0bed211381603b36cac76fa1416cbdd2961544
Haowei Pei <2359365350@Qq.com>
2021-07-30 13:11:54 +0100

    chapter 9

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
          views.add_page, name='add_page'),
     path('register/', views.register, name='register'), # New mapping
     path('login/', views.user_login, name='login'),
+    path('restricted/', views.restricted, name='restricted'),
     path('logout/', views.user_logout, name='logout'),
 ]
